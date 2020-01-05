222bd7bef7a4d7136be726032193405328d9af4a
brian <brian.rivera.proboste@gmail.com>
2020-01-05 00:45:32 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://strider:Y1S6z7htAV3xMnRX@cluster0-g9igt.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 
