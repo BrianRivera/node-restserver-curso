656734b8369c33d3c9f945c7af1d852e9899d09a
brian <brian.rivera.proboste@gmail.com>
2020-01-15 17:50:46 -0400

    mantenedor categoria

---
+++ server/models/categoria.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema;
+
+let categoriaSchema = new Schema({
+    descripcion: {
+        type: String,
+        unique: true,
+        required: [true, 'la descruipcion es nesesaria']
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+});
+
+categoriaSchema.plugin(uniqueValidator, { message: '{PATH}    debe de ser  unico' })
+
+module.exports = mongoose.model('Categoria', categoriaSchema);
